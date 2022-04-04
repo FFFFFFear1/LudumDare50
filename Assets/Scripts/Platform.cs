c38a2a3ee6fdb1acd770598bc92e633247915774
KillRyusha <k.gerasimov@inbox.ru>
2022-04-04 21:39:14 +0300

    добавил таймер жизни блока

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -1,8 +1,14 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Platform : MonoBehaviour
 {
     public PlatformType type;
+    [SerializeField] private float _lifeTime = 5;
+    [SerializeField] private Image _timerImage;
+    [SerializeField] private TMP_Text _timerText;
     private void OnMouseDown()
     {
         if(Input.GetMouseButton(0))
@@ -10,4 +16,24 @@ public class Platform : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void OnEnable()
+    {
+        StartCoroutine(StartPlatformBreak());
+    }
+    private IEnumerator StartPlatformBreak()
+    {
+        _timerImage.fillAmount = 1;
+        float timer = _lifeTime;
+        float seconds = _lifeTime / 60;
+        while (timer >= 0)
+        {
+            _timerImage.fillAmount = timer/_lifeTime;
+            _timerText.text = (int)timer+1 + "";
+            timer -= seconds;
+            yield return new WaitForSeconds(seconds);
+        }
+        gameObject.SetActive(false);
+    }
+
+
 }
